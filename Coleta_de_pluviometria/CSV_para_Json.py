eb8d9b90127230d1b1253c72e8b79bc7eac6d930
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-05-11 11:59:02 -0300

    Le os csvs historicos e salva chuva num unico json

---
+++ Coleta_de_pluviometria/CSV_para_Json.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import json
+import os
+
+
+def get_mm(mm):
+    mm = str(mm)
+    mm = mm.replace(",", ".")
+    if not mm or "-" in mm:
+        mm = 0.0
+    return float(mm)
+
+
+estados = [
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RS",
+    "RO",
+    "RR",
+    "SC",
+    "SP",
+    "SE",
+    "TO",
+]
+dados = {uf: {} for uf in estados}
+
+DIR = "./Coleta_de_pluviometria/CSVs"
+files = os.listdir(DIR)
+for file in files:
+    path = f"{DIR}/{file}"
+    info = file.split("_")
+    uf = info[2]
+    estacao = info[4]
+
+    with open(path, "r") as text_file:
+        linhas = text_file.readlines()
+        n = len(linhas)
+
+    with open(path, "w") as text_file:
+        for i in range(n):
+            if i > 7:
+                text_file.write(linhas[i])
+
+    tabela = pd.read_csv(path, sep=";")
+    tabela = tabela.iloc[:, [0, 2]]
+
+    tabela.set_axis(["Data", "Precipitacao"], axis="columns", inplace=True)
+    tabela["Precipitacao"] = tabela["Precipitacao"].apply(get_mm)
+
+    ano = tabela["Data"][0][:4]
+    print(uf, estacao, ano)
+
+    mensal = {}
+    for mes in range(1, 13):
+        mes = str(mes).zfill(2)
+        tabela_mes = tabela[
+            list(
+                map(
+                    lambda x: x.startswith(f"{ano}-{mes}")
+                    or x.startswith(f"{ano}/{mes}"),
+                    tabela.Data,
+                )
+            )
+        ]
+        total = tabela_mes["Precipitacao"].sum()
+        mensal[mes] = round(total, 2)
+
+    if estacao not in dados[uf]:
+        dados[uf][estacao] = {}
+    dados[uf][estacao][ano] = mensal
+
+
+with open("./Coleta_de_pluviometria/Pluviometria_Brasil.json", "w") as file:
+    json.dump(dados, file)

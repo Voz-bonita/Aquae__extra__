f1b20adafef686228ab791f7d1c3450f73d4d31e
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-05-12 09:44:42 -0300

    Coleta IPCA na db no IBGE

---
+++ Coleta_IPCA.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import date
+
+
+hoje = date.today()
+ano = hoje.year
+mes = hoje.month
+mes_mm = str(mes - 1).zfill(2)
+
+
+url = "https://sidra.ibge.gov.br/Ajax/JSon/Valores/1/1737"
+payload = {
+    "params": f"t/1737/f/c/h/n/n1/all/V/2266/P/{ano}{mes_mm}/d/v2266 13",
+    "versao": "-1",
+    "desidentifica": "false",
+}
+
+response = requests.post(url, data=payload)
+print(response.status_code)
+print(response.content)

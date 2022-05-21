40c7cd0556357757939147ef52781ba0094a946a
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-05-21 15:16:20 -0300

    Busca apenas as estacoes validadas

--- Coleta_de_pluviometria/Via_API.py
+++ Coleta_de_pluviometria/Via_API.py
@@ -11,20 +11,22 @@ def get_mm(row):
     return float(mm)
 
 
-ano_atual = date.today().year
-ano_inicial = ano_atual - 5
+with open("Coleta_de_pluviometria/Cidades_Track.csv", "r") as file:
+    estacoes = file.read().split(",")
 
-DF = ["A045", "A001", "A042", "A046", "A047"]
+
+ano_atual = date.today().year
+ano_inicial = ano_atual - 1
+
 anual = {}
 for ano in range(ano_inicial, ano_atual):
-    print("-" * 200)
     print(f"Ano: {ano}")
     mensal = {}
     for mes in range(1, 13):
         print(f"Mes: {mes}")
         ultimo_dia = monthrange(ano, mes)[1]
         mes = str(mes).zfill(2)
-        url = f"https://apitempo.inmet.gov.br/estacao/{ano}-{mes}-01/{ano}-{mes}-{ultimo_dia}/{DF[0]}"
+        url = f"https://apitempo.inmet.gov.br/estacao/{ano}-{mes}-01/{ano}-{mes}-{ultimo_dia}/{estacoes[0]}"
         with urllib.request.urlopen(url) as url:
             data = json.loads(url.read().decode())
             mm = list(map(get_mm, data))
@@ -36,5 +38,5 @@ for ano in range(ano_inicial, ano_atual):
 
 
 estacao = data[0]["DC_NOME"]
-with open(f"{estacao}.json", "w") as file:
-    json.dump(anual, file)
+# with open(f"{estacao}.json", "w") as file:
+#     json.dump(anual, file)

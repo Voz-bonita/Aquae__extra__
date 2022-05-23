03f7574927471562079374d689c026594a1b16fa
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-05-23 11:31:13 -0300

    Pega corretamente o mês anterior e verifica o SSL

--- Coleta_IPCA.py
+++ Coleta_IPCA.py
@@ -1,21 +1,22 @@
 import requests
-from datetime import date
+from datetime import date, timedelta
 import json
 
 hoje = date.today()
-ano = hoje.year
-mes = hoje.month
-mes_mm = str(mes - 1).zfill(2)
+primeiro = hoje.replace(day=1)
+mes_passado = primeiro - timedelta(days=1)
+ano = mes_passado.year
+mes = str(mes_passado.month).zfill(2)
 
 
 url = "https://sidra.ibge.gov.br/Ajax/JSon/Valores/1/1737"
 payload = {
-    "params": f"t/1737/f/c/h/n/n1/all/V/2266/P/{ano}{mes_mm}/d/v2266 13",
+    "params": f"t/1737/f/c/h/n/n1/all/V/2266/P/{ano}{mes}/d/v2266 13",
     "versao": "-1",
     "desidentifica": "false",
 }
 
-response = requests.post(url, data=payload)
+response = requests.post(url, data=payload, verify="ibge.pem")
 print(response.status_code)
 print(response.content)
 

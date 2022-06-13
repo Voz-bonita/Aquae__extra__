bd0be154929d8a6d3688ce4120dee80fc1462670
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-06-13 10:33:10 -0300

    Ano escalável 3 anos antes

--- Coleta_Tarifa/SNIS.py
+++ Coleta_Tarifa/SNIS.py
@@ -1,10 +1,11 @@
 from tabula import read_pdf
+from datetime import datetime
 
 
-snis = "http://www.snis.gov.br/downloads/diagnosticos/ae/2019/Diagnostico-SNIS-AE-2019-Capitulo-12.pdf"
+ano = datetime.today().year - 3
+snis = f"http://www.snis.gov.br/downloads/diagnosticos/ae/{ano}/Diagnostico-SNIS-AE-{ano}-Capitulo-12.pdf"
 df = read_pdf(
     input_path=snis,
     pages=4,
     area=[290, 80, 800, 600],
 )[0]
-print(df.iloc[:, 0:2])
